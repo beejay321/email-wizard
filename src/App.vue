a36cb5de6078ccb631ad35433d87a927ad9d6ed1
Oluwabusola <jibodu.busola@gmail.com>
2023-07-12 15:11:52 +0200

    refactored App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-
-    </div>
+  <div id="app" class="container">
+    <EmailWizard />
+  </div>
 </template>
 
-<script setup>
+<script>
+import EmailWizard from "./components/EmailWizard.vue";
 
+export default {
+  components: {
+    EmailWizard,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Fasthand&family=Rubik:wght@300&display=swap");
 
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 900px;
+  margin: 0 auto;
+  width: 100vw;
+  background-color: rgb(245, 245, 245);
+}
 </style>
