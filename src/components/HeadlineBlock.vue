e1a33b2d01a4bcfcac53f651bad0b1b0554baa25
Oluwabusola <jibodu.busola@gmail.com>
2023-07-12 15:12:23 +0200

    modified headline block

--- src/components/HeadlineBlock.vue
+++ src/components/HeadlineBlock.vue
@@ -1,25 +1,30 @@
 <template>
-    <div>
-      <input class="headline" type="text" v-model="headline" placeholder="Enter headline" />
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        headline: '',
-      }
-    }
-  }
-  </script>
-  <style lang="scss" scoped>
-  .headline {
-    margin-top: 5px;
-    border: none;
-    width: 20%;
-    height: 30px;
-    font-size:25px ;
-    // background-color: rgb(255, 255, 255);
-  }
-  </style>
+  <div>
+    <input
+      class="headline"
+      type="text"
+      v-model="headline"
+      placeholder="Enter headline"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headline: "",
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.headline {
+  margin-top: 5px;
+  border: none;
+  height: 30px;
+  min-width: auto;
+  font-size: 25px;
+  // background-color: rgb(255, 255, 255);
+}
+</style>
